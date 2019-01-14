ed6a540a59fceddee4bc35ac028428b72266d3c3
Bjorn Stadheim <stadheimb@gmail.com>
2019-01-14 14:20:03 +0100

    Valider token

--- cucumber/features/step_definitions/stepdefs.js
+++ cucumber/features/step_definitions/stepdefs.js
@@ -12,11 +12,12 @@ const {
     hentToken
 } = require('/support/fasit')
 
-When('jeg ber om et token fra {string}', function(env, done) {
+When('jeg ber om et token fra {string}', function (env, done) {
     console.log("Henter token for env", env)
     hentToken(env)
         .then(response => {
             this.token = response;
+            this.tokenJwt = jwt.decode(this.token)
             done()
         })
         .catch(err => {
@@ -26,21 +27,19 @@ When('jeg ber om et token fra {string}', function(env, done) {
         })
 });
 
-Then('skal token være gyldig', function(done) {
-    var tok = jwt.decode(this.token)
-    console.log(tok)
+Then('skal token være gyldig', function () {
+    var tok = this.tokenJwt
     assert.ok(tok.tokenName == "id_token", tok)
-    done()
 });
 
-Then('token skal ha følgende properties:', function(table) {
-    var tok = jwt.decode(this.token)
+Then('token skal ha følgende properties:', function (table) {
+    var tok = this.tokenJwt
     table.rawTable.forEach(item => {
-	var key = item[0]
-	var value = item[1]
-	console.log("Sjekk", key, value)
-	if(!tok[key] || tok[key] != value) {
-	    throw "Bad or missing property: " + key;
-	}
-    }) 
+        var key = item[0]
+        var value = item[1]
+        console.log("Sjekk", key, value)
+        if (!tok[key] || tok[key] != value) {
+            throw "Bad or missing property: " + key;
+        }
+    })
 });
