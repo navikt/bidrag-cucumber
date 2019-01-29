b30fe28c0482abdaa7b8e4accec3cfd55dfbc705
Bjorn Stadheim <stadheimb@gmail.com>
2019-01-29 12:31:50 +0100

    Ta med namespace

--- RunCucumberTest.groovy
+++ RunCucumberTest.groovy
@@ -2,6 +2,7 @@ node {
    def repo = "navikt"
    def application = env.Application
    def branch = env.Branch
+   def ns = env.Namespace
  
     stage("#1: Checkout code") {
         cleanWs()
@@ -18,7 +19,7 @@ node {
     stage("#2 Cucumber tests") {
         println("[INFO] Run cucumber tests")
         withCredentials([usernamePassword(credentialsId: 'naisUploader', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-            sh "docker run --rm -e fasit_user=${env.USERNAME} -e fasit_pass='${env.PASSWORD}' -v ${env.WORKSPACE}/cucumber:/cucumber bidrag-dokument-cucumber"
+            sh "docker run --rm -e environment=${ns} -e fasit_user=${env.USERNAME} -e fasit_pass='${env.PASSWORD}' -v ${env.WORKSPACE}/cucumber:/cucumber bidrag-dokument-cucumber"
         }
 
         if(fileExists('cucumber/cucumber.json')) {
