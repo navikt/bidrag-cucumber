472f4bb817870ddd1c3d2462605d7af5fb662e88
Bjorn Stadheim <stadheimb@gmail.com>
2019-02-18 08:58:58 +0100

    Samle funkjsonelt relaterte fixtures i samme fil

--- cucumber/features/step_definitions/dokument.js
+++ cucumber/features/step_definitions/dokument.js
@@ -3,28 +3,4 @@ const util = require('util')
 const { When, Then } = require('cucumber')
 const { kallFasitRestService, attachJSON, attachText } = require('fasit')
 
-function journalpostSuffix(saksnummer, fagomrade) {
-    return util.format("/sakjournal/%s?fagomrade=%s", saksnummer, fagomrade)
-}
-
-When('jeg henter journalposter for sak {string} med fagområde {string}', function(saksnummer, fagomrade, done) {
-    var pathAndParam = journalpostSuffix(saksnummer, fagomrade)
-    attachText(this, `Kaller journalposter med ${pathAndParam}`)
-    kallFasitRestService(this.alias, pathAndParam)
-        .then(response => {
-            this.response = response
-            assert(this.response != null, "Intet svar mottatt fra tjenesten")
-            assert(undefined === this.response.errno, "Feilmelding: " + this.response.errno)
-            done()
-        })
-        .catch(err => {
-            done(err)
-        })
-})
-
-Then('skal resultatet være et journalpost objekt', function() {
-    var data = this.response ? this.response.data : null
-    assert.ok(data != null, "posten finnes ikke")
-    assert.ok(data.jp_id != null, "journalposten mangler påkrevde properties")
-})
 
