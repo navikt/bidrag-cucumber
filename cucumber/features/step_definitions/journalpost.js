675ab16d603e1c5b5f34e98926204f21b50c71c5
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-02-19 11:39:58 +0100

    httpPost -> httpPut, sak -> saksjournal

--- cucumber/features/step_definitions/journalpost.js
+++ cucumber/features/step_definitions/journalpost.js
@@ -9,31 +9,20 @@ function journalpostSuffix(saksnummer) {
 }
 
 /**
- * bidrag-dokument bruker denne URL for å få journalposter for en sak
+ * URL for å få journalposter for en sak
  * 
  * @param {String} saksnummer 
  * @param {String} fagomrade 
  */
-function sakSuffixBD(saksnummer, fagomrade) {
+function sakSuffix(saksnummer, fagomrade) {
     return util.format("/sakjournal/%s?fagomrade=%s", saksnummer, fagomrade)
 }
 
 /**
- * bidrag-dokument-journalpost bruker denne URL for å få journalposter for en sak
- * 
- * @param {String} saksnummer 
- * @param {String} fagomrade 
- */
-function sakSuffixBDJ(saksnummer, fagomrade) {
-    return util.format("/sak/%s?fagomrade=%s", saksnummer, fagomrade)
-}
-
-/**
- * Bruker enten bidrag-dokumnet eller bidrag-dokument-journalpost sin URL for å hente journalposter
- * for et gitt saksnummer i et fagområde.
+ * Henter journalposter for et gitt saksnummer i et fagområde.
  */
 When('jeg henter journalposter for sak {string} med fagområde {string}', function(saksnummer, fagomrade, done) {
-    pathAndParam = this.alias == "bidragDokument" ? sakSuffixBD(saksnummer, fagomrade) :  sakSuffixBDJ(saksnummer, fagomrade)
+    pathAndParam = sakSuffix(saksnummer, fagomrade)
     console.log("henter journalposter", this.alias, pathAndParam)
     kallFasitRestService(this.alias, pathAndParam)
         .then(response => {
