cb78920ecf788c0040a828ce4fdbb8b5de77acd4
stadheimb-navikt <bjorn.stadheim@nav.no>
2019-05-10 13:08:47 +0200

    Legg til detaljer for hver feature

--- slackMessage.js
+++ slackMessage.js
@@ -7,7 +7,7 @@ const js = JSON.parse(String(fs.readFileSync('cucumber/cucumber.json')))
 var msg = [
 ]
 
-msg.push(`Cucumber Testresultater - build ${process.env.BUILD_DISPLAY_NAME}`)
+msg.push(`Cucumber Testresultater (${process.env.BUILD_NUMBER}) - http://a34apvl00118.devillo.no:8080/job/bidrag-cucumber/${process.env.BUILD_NUMBER}`)
 js.forEach(feature => {
     msg.push('  Feature: ' + feature.name)
     failed = 0
