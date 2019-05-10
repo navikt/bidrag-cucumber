893a3918d7bd6f3581a38867ac684a5dabd902ae
stadheimb-navikt <bjorn.stadheim@nav.no>
2019-05-10 11:24:43 +0200

    Sjekk alle features

--- slackMessage.js
+++ slackMessage.js
@@ -7,14 +7,17 @@ const js = JSON.parse(String(fs.readFileSync('cucumber/cucumber.json')))
 var msg = [
 ]
 
-js[0].elements.forEach(element => {
-    if (!element.steps.every(step => {
-        return step.result.status == "passed"
-    })) {
-        msg.push(`[FAILED] ${element.id}`)
-    } else {
-        msg.push(`[  OK  ] ${element.id}`)
-    }
-});
+js.forEach(feature => {
+    msg.push(feature.name)
+    feature.elements.forEach(element => {
+        if (!element.steps.every(step => {
+            return step.result.status == "passed"
+        })) {
+            msg.push(`  [FAILED] ${element.id}`)
+        // } else {
+        //     msg.push(`  [  OK  ] ${element.id}`)
+        }
+    })
+})
 
 console.log(msg.join('\n'))
