e9cb434ce7ffaa8c57f91540fe656b8d3f82dbaa
stadheimb-navikt <bjorn.stadheim@nav.no>
2019-07-09 10:22:10 +0200

    Lag ny journalpost test

---
+++ cucumber/features/step_definitions/testdata.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const util = require('util');
+const {
+    When
+} = require('cucumber');
+const {
+    httpPost
+} = require('fasit');
+
+
+When('jeg lager ny journalpost', function (body, done) {
+    httpPost(this, this.alias, "", body)
+        .then(response => {
+            this.response = response
+            assert(this.response != null, "Intet svar mottatt fra tjenesten");
+            assert(undefined === this.response.errno, "Feilmelding: " + this.response.errno);
+            done()
+        })
+        .catch(err => {
+            done(err)
+        })
+});
