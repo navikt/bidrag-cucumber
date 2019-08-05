760fedb43d2200b3e4e6dfa743e1324d99108671
Millad Dagdoni <milladdagdoni@Millads-MacBook-Pro.local>
2019-08-05 11:58:27 +0200

    add ukjent avvik test

--- cucumber/features/step_definitions/avvik.js
+++ cucumber/features/step_definitions/avvik.js
@@ -32,3 +32,16 @@ When('jeg kaller bestill reskanning endpoint med journalpostID {string}', functi
             done(err)
         })
 });
+
+when("jeg kaller bestill endpoint med avvik {string} og journalpostID {string}", function(avvikType,journalostid,done){
+    httpPost(this, this.alias, `/journalpost/avvik/${journalpostid}`,{"avvikType":avvikType, "enhetsnummer":"4806" })
+        .then(response => {
+            this.response = response;
+            assert(this.response != null, "Intet svar mottatt fra tjenesten");
+            assert(undefined === this.response.errno, "Feilmelding: " + this.response.errno);
+            done()
+        })
+        .catch(err => {
+            done(err)
+        })
+});
